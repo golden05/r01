cbf479b90609b4c1cc63d748849ebd4b05400f04
golden05 <golden05@gmail.com>
2023-02-07 16:52:10 +0800

    d2day

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,30 @@
 import * as React from "react";
 
-const Item = props => (
+const Item = ({ title, url, author, num_comments, points }) => (
   <li>
     <span>
-      <a href={props.item.url}>{props.item.title}</a>
+      <a href={url}>{title}</a>
     </span>
-    <span>{props.item.author}</span>
-    <span>{props.item.num_comments}</span>
-    <span>{props.item.points}</span>
+    <span>{author}</span>
+    <span>{num_comments}</span>
+    <span>{points}</span>
   </li>
 );
 
-const List = props => (
+const List = ({ list }) => (
   <ul>
-    {props.list.map(item => (
-      <Item key={item.objectID} item={item} />
+    {list.map(({ objectID, ...item }) => (
+      <Item key={objectID} {...item} />
     ))}
   </ul>
 );
 
-const Search = props => (
+const Search = ({ search, onSearch }) => (
   <div>
     <label htmlFor="search">Search: </label>
-    <input id="search" type="text" onChange={props.onSearch} />
+    <input id="search" type="text" value={search} onChange={onSearch} />
     <p>
-      Searching for <strong>{props.search}</strong>
+      Searching for <strong>{search}</strong>
     </p>
   </div>
 );
@@ -37,7 +37,7 @@ const App = () => {
       author: "Jordan Walke",
       num_comments: 3,
       points: 4,
-      objectID: 0
+      objectID: 0,
     },
     {
       title: "Redux",
@@ -45,15 +45,15 @@ const App = () => {
       author: "Dan Abramov, Andrew Clark",
       num_comments: 2,
       points: 5,
-      objectID: 1
-    }
+      objectID: 1,
+    },
   ];
 
   const [searchTerm, setSearchTerm] = React.useState("");
 
-  const handleSearch = event => setSearchTerm(event.target.value);
+  const handleSearch = (event) => setSearchTerm(event.target.value);
 
-  const searchedStories = stories.filter(story =>
+  const searchedStories = stories.filter((story) =>
     story.title.toLowerCase().includes(searchTerm.toLowerCase())
   );
 
